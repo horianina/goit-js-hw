4ae03ff5f94f8a3b290c142b947cbb02ed3c7bee
horianina <anna.goryanina88@gmail.com>
2020-05-12 21:25:41 +0300

    added task_2.js

--- module1/tasks/task_1.js
+++ module1/tasks/task_1.js
@@ -1,16 +1,16 @@
-'use strict';
+//'use strict';
 
-let price = 1000;
-console.log(price);
+//let price = 1000;
+//console.log(price);
 
-const name = 'Генератор защитного поля';
-console.log(name);
+//const name = 'Генератор защитного поля';
+//console.log(name);
 
-const message = `Выбран ${name}, цена за штуку ${price} кредитов.`;
+//const message = `Выбран ${name}, цена за штуку ${price} кредитов.`;
 
-console.log(message);
+//console.log(message);
 
-price = 2000;
-const sameMessage = `Выбран ${name}, цена за штуку ${price} кредитов.`;
+//price = 2000;
+//const sameMessage = `Выбран ${name}, цена за штуку ${price} кредитов.`;
 
-console.log(sameMessage);
+//console.log(sameMessage);
--- module1/tasks/task_2.js
+++ module1/tasks/task_2.js
@@ -1,15 +1,15 @@
-//'use strict';
+'use strict';
 
-//const total = 100;
-//const ordered = 50;
-//const ordered = 120;
+const total = 100;
+const ordered = 50;
+const ordered = 120;
 
-//let message;
+let message;
 
-//if (ordered > total) {
-//  message = 'На складе недостаточно твоаров!';
-//} else {
-// message = 'Заказ оформлен, с вами свяжется менеджер';
-//}
+if (ordered > total) {
+  message = 'На складе недостаточно твоаров!';
+} else {
+  message = 'Заказ оформлен, с вами свяжется менеджер';
+}
 
-//console.log(message);
+console.log(message);
