137d75f32e40865a56723cd6d81ada00ec85e7f1
horianina <anna.goryanina88@gmail.com>
2020-05-13 18:07:06 +0300

    added task_6

---
+++ module1/tasks/task_6.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let input = prompt('Введите число');
+let total = 0;
+
+while (input) {
+  total += input;
+  input = prompt('Введите число');
+}
+
+alert('Общая сумма чисел равна [' + total + ']');
