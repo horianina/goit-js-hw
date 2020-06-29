f32974aac1cf7ab421596b76cda6bd5873659f1e
horianina <anna.goryanina88@gmail.com>
2020-06-29 09:51:18 +0300

    added module6

---
+++ module6/script.js
@@ -0,0 +1,10 @@
+import './tasks/task_1.js';
+//import './tasks/task_2.js';
+//import './tasks/task_3.js';
+//import './tasks/task_4.js';
+//import './tasks/task_5.js';
+//import './tasks/task_6.js';
+//import './tasks/task_7.js';
+//import './tasks/task_8.js';
+//import './tasks/task_9.js';
+//import './tasks/task_10.js';
